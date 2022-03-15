2afe6d8f81cef00479da91531ee5a228ba77d667
Andy Monzón <andyml2639@gmail.com>
2022-03-15 09:52:26 -0600

    Datos personales en un print
    se colocaron mis datos personales de estudiante en un print

--- hola_Phython.py
+++ hola_Phython.py
@@ -5,3 +5,5 @@ while contador <= 10:
     contador += 1
     
 print("\nFelicidades, va muy bien con su evaluación parcial. Att. Ing. Miguel")
+
+print("\nNombre: Andy Isaac Monzón López, carné: 1530522")
